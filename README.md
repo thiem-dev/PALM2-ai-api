0fbed29cb708ddebdece8f395dd34213c75a3c53
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-17 15:14:14 -0500

    sample bard code.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sample PALM2 prompt and response app.
+- d
+
+
+### Helpful Links
+
+- Get API Key: https://makersuite.google.com/app/apikey 
+
+- Hello World prompt: https://developers.generativeai.google/tutorials/chat_node_quickstart
---
+++ index.js
@@ -0,0 +1,38 @@
+
+// bard stuff to exported to main.js
+const { DiscussServiceClient } = require("@google-ai/generativelanguage");
+const { GoogleAuth } = require("google-auth-library");
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const MODEL_NAME = "models/chat-bison-001";
+const API_KEY = process.env.GOOG_API_KEY;
+
+const client = new DiscussServiceClient({
+  authClient: new GoogleAuth().fromAPIKey(API_KEY),
+});
+
+
+// Read the JSON file
+const rawData = fs.readFileSync('sample.json');
+const recipeData = JSON.parse(rawData);
+
+
+async function main() {
+  const result = await client.generateMessage({
+    model: MODEL_NAME, // Required. The model to use to generate the result.
+    temperature: 0.5, // Optional. Value `0.0` always uses the highest-probability result.
+    candidateCount: 1, // Optional. The number of candidate results to generate.
+    prompt: {
+      // Required. Alternating prompt/response messages.
+      messages: [{ content: `Give me a sweet potato fries recipe. Only respond in a json format with title, description, ingredients, and instructions like this: ${recipeData} 
+        Do not provide anything else` }],
+    },
+  });
+
+  console.log(result[0].candidates[0].content);
+}
+
+main();
